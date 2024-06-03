054412126e48fb6f411cb08059244cadf167a294
nonomarino <geomari147@gmail.com>
2024-06-03 18:21:06 +0300

    final

--- backroundWin.py
+++ backroundWin.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout,     QMainWindow, QSizePolicy)
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout, QMainWindow, QSizePolicy)
 from PyQt5.QtGui import QIcon, QPixmap
 
 class BaseWindow(QMainWindow):
